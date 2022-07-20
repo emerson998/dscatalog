b59e414d8316f7367ecabd60cb0c45b6c85f3933
emerson998 <oemerson1000@gmail.com>
2022-07-19 21:18:51 -0300

    Create project

---
+++ src/main/java/ds/com/br/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package ds.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
